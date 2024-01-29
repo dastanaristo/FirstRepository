42695de336a3cdeeeefcb14a560608c6bb2de3ca
dastanaristo <158052355+dastanaristo@users.noreply.github.com>
2024-01-29 20:24:39 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "SUS": "Sesuatu yang tidak masuk akal",
+            "SKIBIDI": "Meme populer yang mucul di youtube tentang toilet yang punya kepala banyank orang memikir meme ini cringe mereka benar",
+            "RIZZ": "..."
+            }
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Maaf,tidak ada kata itu")
+
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Maaf,tidak ada kata itu")
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Maaf,tidak ada kata itu")
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Maaf,tidak ada kata itu")
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Maaf,tidak ada kata itu")
